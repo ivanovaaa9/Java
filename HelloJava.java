3ae982a1540bfe8a21b8bd28045729eb6907b579
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-29 11:59:13 +0300

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bg.unwe;
+
+/**
+ *
+ * @author user
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hello from UNWE! ");
+    System.out.println("Using ints: ");
+int x = 3;
+int y = 10;
+int addition = x + y;
+int diff = x - y;
+int mult = x * y;
+int div = y / x;
+int mod = y % x;
+System.out.println(addition);
+System.out.println(diff);
+System.out.println(mult);
+System.out.println(div);
+System.out.println(mod);
+
+System.out.println("Using double's: ");
+
+double a = 3;
+double b = 10;
+double addition2 = a + b;
+double diff2 = a - b;
+double mult2 = a * b;
+double div2 = b / a;
+double mod2 = b % a;
+System.out.println(addition2);
+System.out.println(diff2);
+System.out.println(mult2);
+System.out.println(div2);
+System.out.println(mod2);
+
+
+    }
+    
+}
