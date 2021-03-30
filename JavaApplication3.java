b1f78272a7534f93dfb31c821b0d48cfb8b164d6
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-30 19:40:56 +0300

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bg.unwe.javaapplication3;
+
+/**
+ *
+ * @author VickyDanny
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args ) {
+       int a1 = 5;
+       int d = 2;
+       int n = 3;
+       
+       int substractedTotal = a1 + (n - 1);
+       int multipliedTotal = (substractedTotal) * d;
+       System.out.print (substractedTotal * multipliedTotal);
+       
+       
+       
+       
+              
+    }
+    
+}
